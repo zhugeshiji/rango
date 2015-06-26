74a61ffa6a79eb359516a95a7008faa6fe2f461d
eason <zhugeshiji@gmail.com>
2015-06-26 17:56:40 +0800

    about 映射

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,4 +1,4 @@
 from django.conf.urls import patterns,url
 from rango import views
 
-urlpatterns = patterns('',url(r'^$',views.index,name='index'))
+urlpatterns = patterns('',url(r'^about',views.index,name='index'))
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,3 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
      return HttpResponse("rango says hey there world")
+
